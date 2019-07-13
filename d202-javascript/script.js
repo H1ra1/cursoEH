34872d5c70b14116fca98dedc71d9100fd69e8fe
Hira <hlira96@gmail.com>
2019-07-13 13:23:15 -0300

    funcao que verifica se ha filmes adequados para idade

--- d202-javascript/script.js
+++ d202-javascript/script.js
@@ -3,14 +3,33 @@
 //     i++
 // }
 
-function find(array, word) {
-    for(palavra of array) {
-        if(word === palavra) {
-           return `Palavra "${palavra}" encontrada na lista`;
-        }
-    } 
-    return 'palavra não encontrada';
-}
+// function find(array, word) {
+//     for(palavra of array) {
+//         if(word === palavra) {
+//            return `Palavra "${palavra}" encontrada na lista`;
+//         }
+//     } 
+//     return 'palavra não encontrada';
+// }
 
-let lista = ['carro', 'folha', 'arvore'];
-console.log(find(lista, 'folha'));
+// let lista = ['carro', 'folha', 'arvore'];
+// console.log(find(lista, 'folha'));
+
+
+// let filmes = [
+//     {nome: 'Harry Potter', classificacao: 14},
+//     {nome: 'Rei Leao', classificacao: 12},
+//     {nome: 'Power Rangers', classificacao: 18},
+//     {nome: 'Mad Max', classificacao: 16},
+//     {nome: 'Ninja Assassino', classificacao: 14}
+// ]
+
+// function assistir(idade) {
+//     for(filme of filmes) {
+//         if(idade >= filme.classificacao) {
+//             console.log(`${filme.nome} (${filme.classificacao})`);
+//         }
+//     }
+// }
+
+// assistir(14);
