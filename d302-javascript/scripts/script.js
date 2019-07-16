ef13081d3b4b8171c7887548d61ee3f273bc208b
Hira <hlira96@gmail.com>
2019-07-16 11:10:19 -0300

    criar funcao para embaralhar

--- d302-javascript/scripts/script.js
+++ d302-javascript/scripts/script.js
@@ -1,3 +1,26 @@
-addEventListener('keypress', e => {
-    console.log(e.keyCode);
-})
+let savedImages = ['n1.jpg', 'n2.jpg', 'n3.jpg', 'n4.jpg', 'n5.jpg', 'n6.jpg', 'n7.jpg', 'n8.jpg'];
+let images = savedImages.concat(savedImages);
+
+function shuffle(array) {
+    let temporary_value;
+    let random_indice;
+
+    for(let i = array.length - 1; i !== 0; i--) {
+        random_indice = Math.floor(Math.random() * i);
+
+        temporary_value = array[i];
+        array[i] = array[random_indice];
+        array[random_indice] = temporary_value;
+
+    }
+
+    return array;
+}
+
+let cards = document.querySelectorAll('.card');
+
+images = shuffle(images);
+
+for(let i in cards) {
+    cards[i].style.backgroundImage = `url('imgs/${images[i]}')`;
+}
