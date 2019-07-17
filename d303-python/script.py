bfad9def923e9acc9bb8221b5e5749b34c546495
Hira <hlira96@gmail.com>
2019-07-17 11:58:27 -0300

    Alterar emprestimo e criar funcao embaralhar

--- d303-python/script.py
+++ d303-python/script.py
@@ -1,3 +1,7 @@
+from random import randint
+
+# ESCADAS ==>
+
 # print('Insira o material da escada:')
 # material = input()
 # print('Insira a quantidade de escadas:')
@@ -10,30 +14,55 @@
 #     print(desenhar)
 #     count += 1
 
-print('Insira seu nome:')
-nome = input()
 
+# EMPRESTIMO ==>
 
-
+# print('Insira seu nome:')
+# nome = input()
+# print('Insira sua idade:')
+# idade = int(input())
+# print('Insira seu salario:')
+# salario = int(input())
+# print('Qual valor do emprestimo?')
+# emprestimo = int(input())
 # print('Parcelar em quantas vezes?')
 # qtdParcelas = input()
 
-if nome:
-    print('Insira sua idade:')
-    idade = int(input())
-    if idade >= 25 and idade <= 55:
-        print('Insira seu salario:')
-        salario = int(input())
-        if salario >= 1000:
-            print('Qual valor do emprestimo?')
-            emprestimo = int(input())
-            if emprestimo <= salario * 15:
-                print(f'Ola, {nome}. Seu emprestimo de {emprestimo} foi aprovado')
-            else:
-                print('Seu emprestimo foi negado pois ultrapassa o valor maximo')
-        else: 
-            print('salario insuficiente')
-    else:
-        print('Idade invalida')
-else: 
-    print('insira seu nome')
+# if nome == '':
+#     print('Preencha o campo nome')
+# elif idade <= 25 and idade >= 55:
+#         print('Idade não aceita')
+# elif salario <= 1000:
+#         print('salario abaixo do desejado')
+# elif emprestimo >= salario * 15:
+#         print('emprestimo acima do limite')
+# else:
+#     print(f'Ola {nome}, o emprestimo no valor de: {emprestimo} foi aprovado')
+
+# EMBARALHAR ==>
+
+arr = [1, 2, 3, 4, 6, 7, 8]
+
+def embaralhar(lista):
+    indice_atual = len(lista)
+    valor_temporario = None
+    indice_aleatorio = None
+    count = 0
+
+    while count != indice_atual:
+        indice_aleatorio = randint(0, indice_atual)
+        indice_atual -= 1
+
+        valor_temporario = lista[indice_atual]
+        lista[indice_atual] = lista[indice_aleatorio]
+        lista[indice_aleatorio] = valor_temporario
+
+    return print(lista)
+
+embaralhar(arr)
+
+
+
+
+
+
