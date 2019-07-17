407e176911954ee89b638338bc9840a582839aa5
Hira <hlira96@gmail.com>
2019-07-17 11:12:56 -0300

    exercicio emprestimo

--- d303-python/script.py
+++ d303-python/script.py
@@ -13,20 +13,27 @@
 print('Insira seu nome:')
 nome = input()
 
-# print('Insira seu salario:')
-# salario = input()
-# print('Qual valor do emprestimo?')
-# emprestimo = input()
+
+
 # print('Parcelar em quantas vezes?')
 # qtdParcelas = input()
 
 if nome:
     print('Insira sua idade:')
-    idade = input()
+    idade = int(input())
     if idade >= 25 and idade <= 55:
-      print('ok')  
+        print('Insira seu salario:')
+        salario = int(input())
+        if salario >= 1000:
+            print('Qual valor do emprestimo?')
+            emprestimo = int(input())
+            if emprestimo <= salario * 15:
+                print(f'Ola, {nome}. Seu emprestimo de {emprestimo} foi aprovado')
+            else:
+                print('Seu emprestimo foi negado pois ultrapassa o valor maximo')
+        else: 
+            print('salario insuficiente')
     else:
         print('Idade invalida')
 else: 
     print('insira seu nome')
-
